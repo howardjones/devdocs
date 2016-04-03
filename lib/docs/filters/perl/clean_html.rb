3a368bdb92ba8c3593f384f1466b4b220eedb126
Howard Jones <howie@thingy.com>
2016-04-03 16:35:54 +0100

    Fixed up sections for all current manual pages

--- lib/docs/filters/perl/clean_html.rb
+++ lib/docs/filters/perl/clean_html.rb
@@ -1,52 +1,14 @@
 module Docs
-  class Perl
-    class CleanHtmlFilter < Filter
-      def call
-        # css('h2').remove # Page Title
-        # css('h3:first-child').remove # Navigation
+	class Perl
+		class CleanHtmlFilter < Filter
+			def call
 
-	css('noscript').remove
-	css('div#recent_pages').remove
-	css('div.mod_az_list').remove
+				css('noscript').remove
+				css('div#recent_pages').remove
+				css('div.mod_az_list').remove
 
-	css('h1').each do |node|
-	   if node.content == 'Perl Programming Language'
-		node.remove
-	   end
+				doc
+			end
+		end
 	end
-
-        #css('div.copy').each do |node|
-        #  node['class'] = '_attribution'
-        #  node.inner_html = %(<p class="_attribution-p">#{node.inner_html.gsub(' Copyright', '<br>\0')}</p>)
-        #end
-
-        #css('a[name]').each do |node|
-        #  node.parent['id'] = node['name']
-        #  node.before(node.children).remove unless node['href']
-        #end
-
-        css('h3').each do |node|
-          if node.content == 'KEYWORDS'
-            node['id'] = 'tmp__'
-            css('#tmp__ ~ a[href*="Keywords"]').each do |link|
-              link.next.remove if link.next.content == ', '
-              link.remove
-            end
-            node.remove
-            next
-          end
-
-          node.name = 'h2'
-          node.content = node.content.capitalize
-        end
-
-        #css('h4').each do |node|
-        #  node.name = 'h3'
-        #  node.content = node.content.capitalize
-        #end
-
-        doc
-      end
-    end
-  end
 end
--- lib/docs/scrapers/perl.rb
+++ lib/docs/scrapers/perl.rb
@@ -1,34 +1,33 @@
 module Docs
-  class Perl < FileScraper
-    self.name = 'Perl'
-    self.type = 'perl'
-    self.release = '5.22.0'
-    self.base_url = 'http://perldoc.perl.org/'
-    self.root_path = 'perl.html'
-    self.dir = '/home/howie/work/perl/perldoc-html'
+	class Perl < FileScraper
+		self.name = 'Perl'
+		self.type = 'perl'
+		self.release = '5.22.0'
+		self.base_url = 'http://perldoc.perl.org/'
+		self.root_path = 'perl.html'
+		self.dir = '/home/howie/work/perl/perldoc-html'
 
-   # self.initial_paths = %w(
-   #   index-modules-A.html)
+		# self.initial_paths = %w(
+		#   index-modules-A.html)
 
 
-#    text_filters.push 'c/fix_urls'
-    html_filters.push 'perl/entries', 'perl/clean_html', 'title'
+		#    text_filters.push 'c/fix_urls'
+		html_filters.push 'perl/entries', 'perl/clean_html', 'title'
 
-    options[:container] = '#content_body'
-    options[:title] = false
-  #  options[:root_title] = 'Perl Programming Language'
-  #  options[:skip] = %w(language/history.html)
-    
-    # there are some links into older (differently formatted) docs
-    # options[:skip_patterns] = [/5\./]
+		options[:container] = '#content_body'
+		options[:title] = false
+		#  options[:root_title] = 'Perl Programming Language'
+		#  options[:skip] = %w(language/history.html)
+				# there are some links into older (differently formatted) docs
+		# options[:skip_patterns] = [/5\./]
 
-    options[:fix_urls] = ->(url) do
-      url.sub! %r{\A.+/http%3A/}, "http://"
-      url
-    end
+		options[:fix_urls] = ->(url) do
+			url.sub! %r{\A.+/http%3A/}, "http://"
+			url
+		end
 
-    options[:attribution] = <<-HTML
-      &copy; perldoc.perl.org<br>
+		options[:attribution] = <<-HTML
+	&copy; perldoc.perl.org<br>
      This documentation is free software; you can redistribute it and/or modify it under the terms of either:
 
         a) the GNU General Public License as published by the Free
