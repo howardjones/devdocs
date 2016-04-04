73ab334cd0edb5accfe9dd61e09390653afc7653
Howard Jones <howie@thingy.com>
2016-04-04 09:53:01 +0100

    Fixed internal anchors

--- lib/docs/filters/perl/clean_html.rb
+++ lib/docs/filters/perl/clean_html.rb
@@ -1,14 +1,22 @@
 module Docs
-	class Perl
-		class CleanHtmlFilter < Filter
-			def call
+  class Perl
+    class CleanHtmlFilter < Filter
+      def call
 
-				css('noscript').remove
-				css('div#recent_pages').remove
-				css('div.mod_az_list').remove
+        css('noscript').remove
+        css('div#recent_pages').remove
+        css('div.mod_az_list').remove
 
-				doc
-			end
-		end
-	end
+        css('a[name]').each do |node|          
+          # puts node
+          if ! node['name'].start_with?('__')
+            node.next_element['id'] = node['name']
+            node.remove
+          end
+        end
+        
+        doc
+      end
+    end
+  end
 end
--- lib/docs/scrapers/perl.rb
+++ lib/docs/scrapers/perl.rb
@@ -1,40 +1,29 @@
 module Docs
-	class Perl < FileScraper
-		self.name = 'Perl'
-		self.type = 'perl'
-		self.release = '5.22.0'
-		self.base_url = 'http://perldoc.perl.org/'
-		self.root_path = 'perl.html'
-		self.dir = '/home/howie/work/perl/perldoc-html'
+  class Perl < FileScraper
+    self.name = 'Perl'
+    self.type = 'perl'
+    self.release = '5.22.0'
+    self.base_url = 'http://perldoc.perl.org/'
+    self.root_path = 'perl.html'
+    self.dir = '/home/howie/work/perl/perldoc-html'
+    
+    html_filters.push 'perl/entries', 'perl/clean_html', 'title'
 
-		# self.initial_paths = %w(
-		#   index-modules-A.html)
+    options[:container] = '#content_body'
+    options[:title] = false
+    # options[:root_title] = 'Perl Programming Language'
 
+    # there are some links into older (differently formatted) docs
+    # and the *delta changelogs aren't really necessary for offline use
+    options[:skip_patterns] = [/5\./, /perl\d+delta/]
 
-		#    text_filters.push 'c/fix_urls'
-		html_filters.push 'perl/entries', 'perl/clean_html', 'title'
-
-		options[:container] = '#content_body'
-		options[:title] = false
-		#  options[:root_title] = 'Perl Programming Language'
-		#  options[:skip] = %w(language/history.html)
-				# there are some links into older (differently formatted) docs
-		# options[:skip_patterns] = [/5\./]
-
-		options[:fix_urls] = ->(url) do
-			url.sub! %r{\A.+/http%3A/}, "http://"
-			url
-		end
-
-		options[:attribution] = <<-HTML
-	&copy; perldoc.perl.org<br>
+    options[:attribution] = <<-HTML
+  &copy; perldoc.perl.org<br>
      This documentation is free software; you can redistribute it and/or modify it under the terms of either:
 
         a) the GNU General Public License as published by the Free
         Software Foundation; either version 1, or (at your option) any
-        later version, or
-
-        b) the "Artistic License" which comes with this documentation.
+        later version, or b) the "Artistic License" which comes with this documentation.
     HTML
   end
 end
